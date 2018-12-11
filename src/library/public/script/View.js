ec57297dcd80b0b5dac24df79d6688a21a91b073
Alex <argentum.tm@gmail.com>
2018-12-12 01:02:27 +0300

    added filters

--- src/library/public/script/View.js
+++ src/library/public/script/View.js
@@ -3,27 +3,33 @@ function View(model, controller) {
     this.model = model;
     this.ctrl = controller;
     this.div = document.getElementById('books');
+    this.filters = document.querySelectorAll('.filter span');
 }
 
 View.prototype.init = function () {
     var that = this;
     //стрелочная форма позволяет не использовать that т.к сохраняет контекст
     this.model.onBooksLoaded.subscribe((books) => {
-        this.renderBooks(books)
-    })
+        this.renderBooks(books);
+    });
+    for (var i=0;i<this.filters.length;i++){
+        this.filters[i].addEventListener('click', function () {
+                that.renderCategories(that.model.books);
+            });
+    }
 }
 View.prototype.renderBooks = function renderBooks(books) {
     books.map((elem) => {
         const book = document.createElement('div');
         const title = document.createElement('div');
         const image = document.createElement('img');
-        const author = document.createElement('span');
+        const author = document.createElement('div');
         const rating = document.createElement('div');
         image.src=elem.src;
         image.classList.add('book-img');
         title.textContent = elem.title;
         title.classList.add('title');
-        author.textContent = elem.author;
+        author.textContent = 'by ' + elem.author;
         author.classList.add('author');
         rating.textContent = elem.rating;
         book.append(image);
@@ -33,4 +39,43 @@ View.prototype.renderBooks = function renderBooks(books) {
         book.classList.add('book');
         this.div.append(book);
     });
+View.prototype.clear = function clear(){
+    while (this.div.childNodes.length !== 0) {
+        this.div.removeChild(this.div.childNodes[0]);
+    }
+}
+View.prototype.renderCategories = function renderCategories(books) {
+    var components=[];
+    switch (event.target.id) {
+        case 'popular':{
+            for (var i=0; i<books.length; i++){
+                if (books[i].rating>=4){
+                    components.push(books[i])
+                }
+            }
+            break;
+        }
+        case 'all-books':{
+            for (var i=0; i<books.length; i++){
+                components.push(books[i])
+            }
+        }
+        case 'most-recent':{
+            for (var i=0; i<books.length; i++){
+                if (books[i].created_at.slice(0,4) === ""+new Date().getFullYear()){
+                    components.push(books[i])
+                }
+            }
+        }
+        case 'free':{
+            for (var i=0; i<books.length; i++){
+                if (books[i].price==0){
+                    components.push(books[i])
+                }
+            }
+        }
+    }
+    this.clear();
+    this.renderBooks(components);
+}
 }
--- src/library/routes/books.js
+++ src/library/routes/books.js
@@ -3,8 +3,11 @@ const router = require('express').Router();
 const BOOKS = [
     { id: 1, title: 'Might', src: 'img/book-cover.png', author: 'Daniel', description: 'Short story', keywords:'short', rating:5, price:100,created_at:new Date(2013,1,1) },
     { id: 2, title: 'Light', src: 'img/book-cover.png', author: 'Michael', description: 'Long story', keywords:'long', rating:4, price:200,created_at:new Date(2013,1,1) },
-    { id: 3, title: 'Height', src: 'img/book-cover.png', author: 'Del', description: 'Any story', keywords:'height', rating:3, price:300,created_at:new Date(2013,1,1)},
-    { id: 4, title: 'Weight', src: 'img/book-cover.png', author: 'Qwe', description: 'Smart story', keywords:'qwe', rating:2, price:400,created_at:new Date(2013,1,1)}
+    { id: 3, title: 'Height', src: 'img/book-cover.png', author: 'Del', description: 'Any story', keywords:'height', rating:3, price:300,created_at:new Date(2018,1,1)},
+    { id: 4, title: 'Weight', src: 'img/book-cover.png', author: 'Qwe', description: 'Smart story', keywords:'qwe', rating:2, price:400,created_at:new Date(2013,1,1)},
+    { id: 5, title: 'Height1', src: 'img/book-cover.png', author: 'Del', description: 'Any story', keywords:'height', rating:3, price:300,created_at:new Date(2018,1,1)},
+    { id: 6, title: 'Height2', src: 'img/book-cover.png', author: 'Del', description: 'Any story', keywords:'height', rating:3, price:300,created_at:new Date(2013,1,1)},
+    { id: 6, title: 'Height3', src: 'img/book-cover.png', author: 'Del', description: 'Any story', keywords:'height', rating:5, price:0,created_at:new Date(2013,1,1)}
 ];
 
 router.get('/', (_, res) => {
